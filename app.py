67ec0b4650c8897cea1b08bfc06fe40b7a0abeaa
rajdeep999 <rajdeep.bsodha444@hotmail.com>
2020-02-02 16:59:17 +0530

    update task feature added

--- app.py
+++ app.py
@@ -44,5 +44,22 @@ def delete(id):
     except:
         return 'There was a problem in deleting the selected task.'
 
+@app.route('/update/<int:id>',methods = ['POST','GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return 'There was an error updating the task'
+    
+    else:
+        return render_template('update.html',task = task)
+
 if __name__ == "__main__":
     app.run(debug=True)
